daa8f065f37bf38df2fe7eb4ee3dc0e1b419c7ca
Samson Yuwono <samson.yuwono@gmail.com>
2017-08-11 23:38:49 +0000

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,16 @@
 class Cart < ActiveRecord::Base
 
+  belongs_to :user
+  has_many :line_items
+  has_many :items, through: :line_items
+
+
+  def add_item
+    self.line_items.create(quantity: 1, cart: @cart)
+  end
+
+  def total
+
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base
 
+  belongs_to :category
+  has_many :line_items
+
+
 end
