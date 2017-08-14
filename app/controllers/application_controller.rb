877fcdd7e12aad35b21b9242c830233365bd6aea
Samson Yuwono <samson.yuwono@gmail.com>
2017-08-14 19:58:52 +0000

    homepage (store) features added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
+     @user= (User.find_by(id: session[:user_id]) || User.new)
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    if current_user.nil?
+      redirect_to store_path
+    end
+  end
+
+
+  helper_method :logged_in?
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
 
+  def new
+    @user= User.new
+  end
+
+
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :encrypted_password)
+  end
 end
