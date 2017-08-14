160e255e0774cb16f0142f0c7aecb6b7addbd21a
Samson Yuwono <samson.yuwono@gmail.com>
2017-08-14 15:57:47 +0000

    add_item method added

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,20 +1,26 @@
 class Cart < ActiveRecord::Base
 
   belongs_to :user
-  has_many :line_items
+  has_many :line_items, :foreign_key => "cart_id"
   has_many :items, through: :line_items
 
 
   def add_item(item_id)
-
+    item = line_items.find_by(item_id: item_id)
+    if item
+      item.quantity += 1
+    else
+      item= line_items.build(item_id: item_id)
+    end
+    item
   end
 
   def total
     @total = 0
     self.line_items.each do |i|
       @total+= i.item.price * i.quantity
-    end
-  @total
-end
+      end
+    @total
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   has_many :carts
-  belongs_to :current_cart, :class_name => "Cart"
+  has_one :current_cart, :class_name => "Cart"
 
   def current_cart
     new_cart = carts.create
-    binding.pry
     self.current_cart_id= new_cart.id
     save
   end
