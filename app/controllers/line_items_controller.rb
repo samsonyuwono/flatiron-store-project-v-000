c69dc946f9616d0749d08571282e4a7d4eb864bc
Samson Yuwono <samson.yuwono@gmail.com>
2017-08-15 15:19:25 +0000

    create method in lineitems controller working

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,6 +3,12 @@ class LineItemsController < ApplicationController
   def create
     current_user.current_cart= Cart.new unless current_user.current_cart
     current_user.save
+    line_item= current_user.current_cart.add_item(params[:item_id])
+    if line_item.save
+      redirect_to cart_path(current_user.current_cart)
+    else
+      redirect_to store_path
+    end
   end
 
 end
