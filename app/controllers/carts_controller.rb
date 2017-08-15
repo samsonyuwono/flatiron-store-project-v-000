e2352cd4fcd0e4f1ffee4551d511adf62cafd2d2
Samson Yuwono <samson.yuwono@gmail.com>
2017-08-15 16:29:23 +0000

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,8 +6,7 @@ class CartsController < ApplicationController
 
   def checkout
     cart= Cart.find(params[:id])
-    cart.checkout
-    current_user.current_cart=
+    current_user.current_cart = nil
     redirect_to cart_path(cart)
   end
 end
